9f94a6e9bd5ff0db4b34f9d87c7da08f9ead37a1
TheSultan <TravelingSultan@gmail.com>
2016-07-25 03:08:14 +0500

    Solved p6.

---
+++ python/p6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+def compute():
+  ans = 0
+  sumsq = 0
+  sqsum = 0
+  for i in range(1,101):
+    sumsq += i**2
+    sqsum += i
+  sqsum = sqsum**2
+  ans = sqsum - sumsq
+  return ans
+
+if __name__ == "__main__":
+  print(compute())
